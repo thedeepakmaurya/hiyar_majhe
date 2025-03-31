edf888e45fc53c1f532555d5faa331f9a517fac8
thedeepakmaurya <deipakmaurya@gmail.com>
2025-03-31 12:39:23 +0530

    fix

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ let textRemoved = true;
 function toggleText() {
     const text = document.querySelector('.hero-text');
     const nav = document.querySelector('.navbar');
+    const call = document.querySelector('.call');
     if (textRemoved) {
         removeText(text);
         nav.classList.add('bg-white');
+        call.classList.add('cst-call');
+        call.classList.remove('call');
     } else {
         addText(text);
         nav.classList.remove('bg-white');
+        call.classList.remove('cst-call');
+        call.classList.add('call');
     }
 }
 
